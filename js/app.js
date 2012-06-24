560ea8e94b03a4e80c1c6fe70d8484d645c40ac1
Michael Pezzi <mike@thespiral.ca>
2012-06-24 10:04:29 -0400

    populate tweets in #posts

--- js/app.js
+++ js/app.js
@@ -3,8 +3,7 @@ $(function(){
   // @see: https://dev.twitter.com/docs/api/1/get/search
   $.twitter({ q: 'epic', include_entities: false }, function(data){
     $.each(data.results, function(i, post){
-      console.log(post);
-      $('#main').append(ich.post(post));
+      $('#posts').append(ich.post(post));
     });
   });
 
