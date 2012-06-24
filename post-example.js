a4d447a8a750b70c3177315b06931602f78be803
Michael Pezzi <mike@thespiral.ca>
2012-06-24 09:53:06 -0400

    added initial twitter functionality

---
+++ post-example.js
@@ -0,0 +1,30 @@
+"created_at":"Thu, 06 Oct 2011 19:36:17 +0000",
+"entities":{
+  "urls":[
+    {
+      "url":"http://t.co/L9JXJ2ee",
+      "expanded_url":"http://bit.ly/q9fyz9",
+      "display_url":"bit.ly/q9fyz9",
+      "indices":[
+        37,
+        57
+      ]
+    }
+  ]
+},
+"from_user":"SFist",
+"from_user_id":14093707,
+"from_user_id_str":"14093707",
+"geo":null,
+"id":122032448266698752,
+"id_str":"122032448266698752",
+"iso_language_code":"en",
+"metadata":{
+  "recent_retweets":3,
+  "result_type":"popular"
+},
+"profile_image_url":"http://a3.twimg.com/profile_images/51584619/SFist07_normal.jpg",
+"source":"&lt;a href=&quot;http://twitter.com/tweetbutton&quot; rel=&quot;nofollow&quot;&gt;Tweet Button&lt;/a&gt;",
+"text":"Reminder: Blue Angels practice today http://t.co/L9JXJ2ee",
+"to_user_id":null,
+"to_user_id_str":null
