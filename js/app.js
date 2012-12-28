7cd4198e791534ef49f1ccd177eeefd65f58dc10
Mike Pezzi <mike@thespiral.ca>
2012-12-28 15:20:41 -0500

    added flipping panels

--- js/app.js
+++ js/app.js
@@ -3,8 +3,13 @@ $(function(){
   // @see: https://dev.twitter.com/docs/api/1/get/search
   $.twitter({ q: 'epic', include_entities: false }, function(data){
     $.each(data.results, function(i, post){
-      $('#posts').append(ich.post(post));
+      $('.panel-container').append(ich.panel(post));
     });
+  });
+
+  // flip panels
+  $('.panel .panel-inner').live('click', function(){
+    $(this).toggleClass('flipped');
   });
 
 });
